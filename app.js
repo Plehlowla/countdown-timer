4df6b6f99e530169c7008be8d3ff4599faa03638
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-04-05 09:59:22 -0500

    added dynamic date,set date

--- app.js
+++ app.js
@@ -21,3 +21,26 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const deadline = document.querySelector('.deadline');
+const giveaway = document.querySelector('.giveaway');
+const item = document.querySelectorAll('.deadline-format h4');
+// console.log(item);
+
+let futureDate = new Date(2022,5,24,12,20,2);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+// console.log(year);
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+// console.log(months[month]);
+month = months[month];
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+console.log(weekday);
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}pm`;
